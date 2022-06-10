c95c731324add32973e989664995c0396540c769
yeslinpardave <81619609+yeslinpardave@users.noreply.github.com>
2022-06-10 06:42:01 -0500

    Add files via upload

---
+++ navbar_prueba_conocimiento/js/main.js
@@ -0,0 +1,9 @@
+
+let menu = document.querySelector('#menu-btn');
+let navbarList = document.querySelector('.nav__list');
+let cosa
+
+menu.onclick=()=>{
+    navbarList.classList.toggle('active');    
+    
+}

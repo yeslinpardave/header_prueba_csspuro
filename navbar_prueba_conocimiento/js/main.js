cf572e4174ad99162da0ddf7bf6c91e496535d07
yeslinpardave <81619609+yeslinpardave@users.noreply.github.com>
2022-06-10 11:47:38 -0500

    Delete navbar_prueba_conocimiento directory

--- navbar_prueba_conocimiento/js/main.js
+++
@@ -1,9 +0,0 @@
-
-let menu = document.querySelector('#menu-btn');
-let navbarList = document.querySelector('.nav__list');
-let cosa
-
-menu.onclick=()=>{
-    navbarList.classList.toggle('active');    
-    
-}
